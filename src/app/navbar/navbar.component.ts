e36a581179c214746a394daba8e6891484bd1f36
FelipeMaldonado10 <afmaldonado10@gmail.com>
2025-07-02 18:10:26 -0500

    Ajuste al navbar(Cerrar al seleccionar)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  closeNavbar() {
+    //Busca el elemento colapsable por su id
+    const navbar = document.getElementById('navbarNav');
+    //Usa el Api de Bootstrap para colapsar el menú
+    //@ts-ignore
+    const collapse= new window.bootstrap.Collapse(navbar, {toggle: false});
+    collapse.hide();
+  }
 }
